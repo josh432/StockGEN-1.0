1fa492a0bf492895081d6f1ee8e0e1eb44979f71
Josh Silverman <jrsilverman71@hotmail.com>
2017-12-14 20:46:33 -0800

    stockgen1.0

---
+++ assets/javascript.js
@@ -0,0 +1,12 @@
+
+// StockGEN JQuery/Javascript
+// StockGEN JQuery/Javascript
+var powerOn = new Audio ("./assets/poweron.mp3");
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+    $('#modal1').modal()
+    powerOn.play();
+
+
+});
